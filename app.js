9fb8e625c20617cfbe619de77b325c8012c71e5c
Natalia <nataliamp87@gmail.com>
2018-09-30 00:47:05 -0300

    research

--- app.js
+++ app.js
@@ -1,5 +1,29 @@
-  /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-  particlesJS.load
+
+
+  // pie
+  var ctxP = document.getElementById('pieChart').getContext('2d');
+  var myPieChart = new Chart(ctxP, {
+    type: 'pie',
+    data: {
+      labels: ['Apple iphone', 'Samsung SM-J700M Galaxy J7 2015'],
+      datasets: [
+        {
+          data: [3.9, 61.6],
+          backgroundColor: ['#9232B3', '#56D9C9', '#FDB45C', '#949FB1', '#4D5360'],
+          hoverBackgroundColor: ['#c564e6', '#83f7ea', '#FFC870', '#A8B3C5', '#616774'],
+        },
+      ],
+    },
+    options: {
+      responsive: true,
+    },
+  });
+
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load
   ('particles-js', 'particles.json', function() {
     console.log('callback - particles.js config loaded');
   });
+
+
+
